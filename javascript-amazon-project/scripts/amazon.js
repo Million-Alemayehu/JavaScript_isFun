db247643770b01253d7550fee333e85e83351d04
Million-Alemayehu <millionalemayehu@gmail.com>
2025-11-16 11:39:24 +0300

    make the cart quantity interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -46,7 +46,9 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}"
+      >
         Add to Cart
       </button>
     </div>
@@ -54,6 +56,37 @@ products.forEach((product) => {
   
 });
 
-console.log(productsHTML);
+
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+
+    let mactchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        mactchingItem = item;
+      }
+    });
+
+    if (mactchingItem) {
+      mactchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+    });
+    }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+  });
+});
